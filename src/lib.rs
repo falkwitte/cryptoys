d2f123c10063fcfeb0c59f41a2347aa592434beb
Falk-Witte <krixcrox@duck.com>
2023-07-26 16:47:02 +0200

    update description

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
-//! Cryptoys is a cryptographic library
+//! Cryptoys is a cryptographic library that contains cryptographic toys, like historical algorithms(rot13, playfair)
+//! or hashing functions like md5.
+//! <br>
+//! <br>
+//! The primary goal of this crate is not to make a fully functioning, save and secure cryptography library (others have done this already),
+//! but to provide fun toys to play around with.
 //!
 //! ##### Features:
 //! - historical ciphers
-//! - modern ciphers
-//! - safe comparission
-//! - key derivation
+//!     - playfair
+//!     - rot13
+//!     - caesar
 //! - hashing function(s)
+//!     - md5
+//!     - sha
 
 pub mod historical;
 mod utils;
